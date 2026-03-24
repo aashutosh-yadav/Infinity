2073a989845a3fad2304613e5ae02f093f6b43a3
Ashutosh Yadav <yadavashutoshh01@gmail.com>
2026-03-24 17:30:42 +0530

    database name was wrong

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, String
 from .database import Base
 
 class URL(Base):
-    __tablename__ = "urls"
+    __tablename__ = "url_shortner"
 
     id = Column(Integer, primary_key=True, index=True)
     short_code = Column(String, unique=True, index=True)
