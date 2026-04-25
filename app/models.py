38aa83a16cbdc1842a9e2f44022e550f41e5c407
Ashutosh Yadav <yadavashutoshh01@gmail.com>
2026-04-25 17:20:07 +0530

    just a typo

--- app/models.py
+++ app/models.py
@@ -6,6 +6,6 @@ class URL(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     short_code = Column(String, unique=True, index=True)
-    # adding index gives id to logurl which makes the loopup O(n)
+    # adding index gives id to logurl which makes the loopup O(log n)
     # adding uniques handles the duplicates 
     long_url = Column(String , unique=True , index=True)
