f178a7b61cb24b50896d10aaccc0a05a54430e9a
aashutosh-yadav <yadavashutoshh01@gmail.com>
2026-05-01 13:56:24 +0530

    cganged the table name typo mistake and some comments

--- app/models.py
+++ app/models.py
@@ -2,10 +2,8 @@ from sqlalchemy import Column, Integer, String
 from .database import Base
 
 class URL(Base):
-    __tablename__ = "url_shortner"
+    __tablename__ = "url_shortener" 
 
     id = Column(Integer, primary_key=True, index=True)
     short_code = Column(String, unique=True, index=True)
-    # adding index gives id to logurl which makes the loopup O(log n)
-    # adding uniques handles the duplicates 
     long_url = Column(String , unique=True , index=True)
